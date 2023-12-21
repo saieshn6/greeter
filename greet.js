4ebfd1aef1fde96f2ad2056c5ab57df76441c920
saieshn6 <ssaiesnnaik6@gmail.com>
2023-12-21 21:18:40 +0530

    some random number

--- greet.js
+++ greet.js
@@ -1,5 +1,26 @@
-function greetme () {
-    console.log('Hi Tony !');
+/**
+ * create a function that genates 10 random greet messages.
+ * and prints them to the console with a line break between each other.
+ */
+
+function randomGreetMessages(){
+    const greetMessages = [
+        'Hello',
+        'Hi',
+        'Hey',
+        'Howdy',
+        'Hola',
+        'new',
+        'File',
+        'Create',
+        'to',
+        'git'
+    ];
+
+    for (let i = 0; i< 10; i++){
+        const randomIndex = Math.floor(Math.random() * greetMessages.length);
+        console.log(greetMessages[randomIndex]);
+    }
 }
 
-greetme();
+randomGreetMessages();
